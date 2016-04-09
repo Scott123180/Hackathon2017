4bbe0fb17e6b576876f395e14b6259037d6dfcad
dr-crow <James.Crowley1@marist.edu>
2016-04-09 02:40:32 -0400

    Fixed the index page

--- db/hackathon.sql
+++ db/hackathon.sql
@@ -20,7 +20,7 @@ INSERT INTO users(name, email, accessLevel)
 VALUES
 ("James Crowley", "james.crowley1@marist.edu", "Super User"),
 ("John Randis", "john.randis1@marist.edu", "Admin"),
-("Scott Hansen", "scott.hansen1@marist.edu", "Moderator"),
+("Scott Hansen", "scott.hansen1@marist.edu", "Moderator")
 
 #Testing the table
 SELECT * FROM users;
